8fed12a1b9d3a3cb2481dccd5f5b78c43f7a5d8d
Polyq MEEEEE <ehc.reader@gmail.com>
2026-01-10 22:24:42 -0700

    C# Prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,62 @@
 using System;
-
+/*
+Write a program that determines the letter grade for a course according to the following scale:
+A >= 90
+B >= 80
+C >= 70
+D >= 60
+F < 60
+*/
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeNumberStr;
+        int gradeNumber;
+        string gradeLetter;
+        bool pass;
+        Console.Write("\nWhat is your grade? ");
+        gradeNumberStr = Console.ReadLine();
+        gradeNumber = int.Parse(gradeNumberStr);
+        if (gradeNumber >= 80)
+        {
+            if (gradeNumber >= 90)
+            {
+                pass = true;
+                gradeLetter = " A!";
+            } else
+            {
+                pass = true;
+                gradeLetter = " B!";
+            }
+        } else
+        {
+            if (gradeNumber >= 70)
+            {
+                pass = true;
+                gradeLetter = " C";
+            } 
+            else if (gradeNumber >= 60)
+            {
+                pass = false;
+                gradeLetter = " D";
+            } 
+            else
+            {
+                pass = false;
+                gradeLetter = " F :(";
+            }
+        }
+
+        Console.WriteLine($"You have a{gradeLetter}");
+        if (pass)
+        {
+            Console.WriteLine($"You Passed the class!\n");
+        } else
+        {
+            Console.WriteLine($"Not passing yet. You got this!\n");
+        }
+        
+        
     }
 }
