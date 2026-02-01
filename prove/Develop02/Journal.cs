71627d0023d8fa62558b57fcf440a250c2708174
Polyq MEEEEE <ehc.reader@gmail.com>
2026-02-01 00:33:05 -0700

    finished assignment specs, no further development yet

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,5 +1,6 @@
 public class Journal
 {
+    static Random rand = new Random();
     public List<string> _prompts;
     public List<Entry> _entries;
 
@@ -10,4 +11,26 @@ public class Journal
             entry.PrintEntry();
         }
     }
+    public Entry WriteEntry()
+    //Write a new entry - Show the user a random prompt (from a list that you create), and save their response, the prompt, and the date as an Entry.
+    {
+        int r = rand.Next(_prompts.Count());
+        string prompt = _prompts[r];
+        Console.WriteLine(prompt);
+        string input = Console.ReadLine();
+
+
+        DateTime theCurrentTime = DateTime.Now;
+        string date_now = theCurrentTime.ToShortDateString();
+
+
+        Entry entry = new Entry();
+        
+        string _date = date_now;
+        string _prompt = prompt;
+        string _input = input;
+
+        return entry;
+    }
+    
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,29 +1,8 @@
 using System;
 /*
-Write a new entry - Show the user a random prompt (from a list that you create), and save their response, the prompt, and the date as an Entry.
-Display the journal - Iterate through all entries in the journal and display them to the screen.
-Save the journal to a file - Prompt the user for a filename and then save the current journal (the complete list of entries) to that file location.
-Load the journal from a file - Prompt the user for a filename and then load the journal (a complete list of entries) from that file. 
-This should replace any entries currently stored in the journal.
-Provide a menu that allows the user choose these options
-Your list of prompts must contain at least five different prompts. Make sure to add your own prompts to the list, but the following are examples to help get you started:
-Who was the most interesting person I interacted with today?
-What was the best part of my day?
-How did I see the hand of the Lord in my life today?
-What was the strongest emotion I felt today?
-If I had one thing I could do over today, what would it be?
-Your interface should generally follow the pattern shown in the video demo below.
-In addition, your program must:
-
-Contain classes for the major components in the program.
-Contain at least two classes in addition to the Program class.
-Demonstrate the principle of abstraction by using member variables and methods appropriately.
-
-For the core requirements you do not need to worry about the following:
-
-Saving your file as a .csv file requires you to handle commas and quotes in the content appropriately. At this point, you can ignore that and just choose a symbol for a separator that you think is unlikely to show up in the content (such as | or ~ or ~|~).
-You do not need to store the date as an actual C# DateTime object in your class or in the file. You can simply store it as a string.
+Finally got it working
 */
+
 // Don't forget to put this at the top, so C# knows where to find the StreamWriter class
 using System.IO; 
 
@@ -36,6 +15,8 @@ class Program
 
         Journal journal1 = new Journal();
 
+        journal1._prompts = ["Who was the most interesting person I interacted with today?","What was the best part of my day?","How did I see the hand of the Lord in my life today?","What was the strongest emotion I felt today?","If I had one thing I could do over today, what would it be?"];
+        journal1._entries = [];
         string filename = "myFile.txt";
         string choice = "0";
         bool done = false;
@@ -49,19 +30,17 @@ class Program
             Console.WriteLine($"5. Quit");
             Console.Write($"What would you like to do? ");
             choice = Console.ReadLine();
+            
             switch (choice){
                 case "1":
-                // new entry object, pick a prompt, store date, prompt, and input
-                DateTime theCurrentTime = DateTime.Now;
-                string dateText = theCurrentTime.ToShortDateString();
+                Entry entry = journal1.WriteEntry();
+                journal1._entries.Add(entry);
                 break;
                 
-
 
                 case "2":
                 journal1.Display();
                 break;
-
 
 
                 case "3":
@@ -76,8 +55,8 @@ class Program
                     string firstName = parts[0];
                     string lastName = parts[1];
                 }
+                Console.WriteLine("\nJournal Loaded!\n");
                 break;
-
 
 
                 case "4":
@@ -87,14 +66,16 @@ class Program
                 using (StreamWriter outputFile = new StreamWriter(filename))
                 {
                     // You can add text to the file with the WriteLine method
-                    outputFile.WriteLine("This will be the first line in the file.");
+                    foreach (Entry i in journal1._entries)
+                    {
+                        
+                    }
+                    Entry a = journal1._entries[0];
                     
-                    // You can use the $ and include variables just like with Console.WriteLine
-                    string color = "Blue";
-                    outputFile.WriteLine($"My favorite color is {color}");
+                    outputFile.WriteLine($"{a._date},{a._prompt},{a._prompt}");
                 }
+                Console.WriteLine("\nJournal Saved!\n");
                 break;
-
 
 
                 case "5":
@@ -103,11 +84,19 @@ class Program
                 break;
 
 
-
                 default:
                 Console.WriteLine("Please select a valid option\n");
                 break;
             }
         }
+    }
+
+    public void Save()
+    {
+        
+    }
+    public void Load()
+    {
+        
     }
 }
